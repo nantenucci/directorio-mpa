78f07660eee6532bb1d77807b72fc55ec1e0e379
Nestor Antenucci <antenucci.nestor@gmail.com>
2026-06-27 10:10:17 -0300

    PWA: manifest, service worker e iconos

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'mpa-2026-v1';
+const ASSETS = [
+  '/directorio-mpa/',
+  '/directorio-mpa/index.html',
+  '/directorio-mpa/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
